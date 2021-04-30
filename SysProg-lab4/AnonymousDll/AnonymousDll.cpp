e7fc3065a17d7daa6af84aae49ac8bb23a4f2cf0
aigoo038 <65832045+aigoo038@users.noreply.github.com>
2021-04-30 18:25:08 +0300

    all-in-one

---
+++ SysProg-lab4/AnonymousDll/AnonymousDll.cpp
@@ -0,0 +1,69 @@
+﻿// AnonymousDll.cpp: определяет процедуры инициализации для библиотеки DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "AnonymousDll.h"
+#include <string>
+#include <iostream>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+BEGIN_MESSAGE_MAP(CAnonymousDllApp, CWinApp)
+END_MESSAGE_MAP()
+
+CAnonymousDllApp::CAnonymousDllApp()
+{	
+
+}
+
+CAnonymousDllApp theApp;
+
+HANDLE hRead, hWrite;
+
+void Cleanup()
+{
+	CloseHandle(hRead);
+	CloseHandle(hWrite);
+}
+
+extern "C" _declspec(dllexport) void __stdcall ProcessClient()
+{
+	static int i = 0;
+	while (true)
+	{
+		std::string s = GetString(hPipe);
+		std::cout << s << std::endl;
+		if (s == "quit")
+		{
+			break;
+		}
+		SendInt(hPipe, i++);
+		DisconnectNamedPipe(hPipe);
+		CloseHandle(hPipe);+
+
+	}
+}
+
+extern "C" _declspec(dllexport) void __stdcall LauncClient()
+{
+	STARTUPINFO si = { sizeof(si) };
+	PROCESS_INFORMATION pi;
+	CreateProcess(NULL, (LPSTR)"lab2cpp.exe", NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
+}
+
+
+
+extern "C" _declspec(dllexport) int __stdcall KillChild()
+{
+	return 0;
+}
+
+BOOL CAnonymousDllApp::InitInstance()
+{
+	CWinApp::InitInstance();
+	return TRUE;
+}
